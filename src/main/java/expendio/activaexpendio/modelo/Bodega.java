a188b57f4b660737cc3d14813cdc81b8e755602c
Usuario <Usuario@DESARROLLO6>
2018-03-08 15:45:39 -0500

    creacion de modelos bodega y doc fuente

---
+++ src/main/java/expendio/activaexpendio/modelo/Bodega.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package expendio.activaexpendio.modelo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Bodega {
+    private String codigo;
+    private String nombre;
+    private boolean estado;
+
+    /**
+     * @return the codigo
+     */
+    public String getCodigo() {
+        return codigo;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @return the estado
+     */
+    public boolean isEstado() {
+        return estado;
+    }
+    
+    
+    
+}
