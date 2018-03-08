6f5656eb94012decf53f3dab48457b0f29d0ed1b
Administrador <Administrador@DESARROLLO1.mshome.net>
2018-03-08 15:44:10 -0500

    Modelo Interno, producto, grupoProducto, usuario

---
+++ src/main/java/expendio/activaexpendio/modelo/Interno.java
@@ -0,0 +1,276 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package expendio.activaexpendio.modelo;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Interno {
+    private String td;
+    private String primerApellido;
+    private String segundoApellido;
+    private String primerNombre;
+    private String segundoNombre;
+    private String nui;
+    private String nacionalidad;
+    private String situacionJuridica;
+    private Date fechaIngreso;
+    private Date fechaSalida;
+    private String delito;
+    private String observaciones;
+    private String estado;
+    private Usuario usuario;
+    private String eliminado;
+    private Timestamp creacion;
+    private Timestamp modificacion;
+
+    /**
+     * @return the td
+     */
+    public String getTd() {
+        return td;
+    }
+
+    /**
+     * @param td the td to set
+     */
+    public void setTd(String td) {
+        this.td = td;
+    }
+
+    /**
+     * @return the primerApellido
+     */
+    public String getPrimerApellido() {
+        return primerApellido;
+    }
+
+    /**
+     * @param primerApellido the primerApellido to set
+     */
+    public void setPrimerApellido(String primerApellido) {
+        this.primerApellido = primerApellido;
+    }
+
+    /**
+     * @return the segundoApellido
+     */
+    public String getSegundoApellido() {
+        return segundoApellido;
+    }
+
+    /**
+     * @param segundoApellido the segundoApellido to set
+     */
+    public void setSegundoApellido(String segundoApellido) {
+        this.segundoApellido = segundoApellido;
+    }
+
+    /**
+     * @return the primerNombre
+     */
+    public String getPrimerNombre() {
+        return primerNombre;
+    }
+
+    /**
+     * @param primerNombre the primerNombre to set
+     */
+    public void setPrimerNombre(String primerNombre) {
+        this.primerNombre = primerNombre;
+    }
+
+    /**
+     * @return the segundoNombre
+     */
+    public String getSegundoNombre() {
+        return segundoNombre;
+    }
+
+    /**
+     * @param segundoNombre the segundoNombre to set
+     */
+    public void setSegundoNombre(String segundoNombre) {
+        this.segundoNombre = segundoNombre;
+    }
+
+    /**
+     * @return the nui
+     */
+    public String getNui() {
+        return nui;
+    }
+
+    /**
+     * @param nui the nui to set
+     */
+    public void setNui(String nui) {
+        this.nui = nui;
+    }
+
+    /**
+     * @return the nacionalidad
+     */
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    /**
+     * @param nacionalidad the nacionalidad to set
+     */
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    /**
+     * @return the situacionJuridica
+     */
+    public String getSituacionJuridica() {
+        return situacionJuridica;
+    }
+
+    /**
+     * @param situacionJuridica the situacionJuridica to set
+     */
+    public void setSituacionJuridica(String situacionJuridica) {
+        this.situacionJuridica = situacionJuridica;
+    }
+
+    /**
+     * @return the fechaIngreso
+     */
+    public Date getFechaIngreso() {
+        return fechaIngreso;
+    }
+
+    /**
+     * @param fechaIngreso the fechaIngreso to set
+     */
+    public void setFechaIngreso(Date fechaIngreso) {
+        this.fechaIngreso = fechaIngreso;
+    }
+
+    /**
+     * @return the fechaSalida
+     */
+    public Date getFechaSalida() {
+        return fechaSalida;
+    }
+
+    /**
+     * @param fechaSalida the fechaSalida to set
+     */
+    public void setFechaSalida(Date fechaSalida) {
+        this.fechaSalida = fechaSalida;
+    }
+
+    /**
+     * @return the delito
+     */
+    public String getDelito() {
+        return delito;
+    }
+
+    /**
+     * @param delito the delito to set
+     */
+    public void setDelito(String delito) {
+        this.delito = delito;
+    }
+
+    /**
+     * @return the observaciones
+     */
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    /**
+     * @param observaciones the observaciones to set
+     */
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+
+    /**
+     * @return the estado
+     */
+    public String getEstado() {
+        return estado;
+    }
+
+    /**
+     * @param estado the estado to set
+     */
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    /**
+     * @return the usuario
+     */
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    /**
+     * @param usuario the usuario to set
+     */
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    /**
+     * @return the eliminado
+     */
+    public String getEliminado() {
+        return eliminado;
+    }
+
+    /**
+     * @param eliminado the eliminado to set
+     */
+    public void setEliminado(String eliminado) {
+        this.eliminado = eliminado;
+    }
+
+    /**
+     * @return the creacion
+     */
+    public Timestamp getCreacion() {
+        return creacion;
+    }
+
+    /**
+     * @param creacion the creacion to set
+     */
+    public void setCreacion(Timestamp creacion) {
+        this.creacion = creacion;
+    }
+
+    /**
+     * @return the modificacion
+     */
+    public Timestamp getModificacion() {
+        return modificacion;
+    }
+
+    /**
+     * @param modificacion the modificacion to set
+     */
+    public void setModificacion(Timestamp modificacion) {
+        this.modificacion = modificacion;
+    }
+    
+    
+    
+    
+    
+}
